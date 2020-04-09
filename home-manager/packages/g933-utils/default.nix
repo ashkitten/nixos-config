6dc2f51682f62a75f922905720c769db13632ef9
ash lea <example@thisismyactual.email>
2020-04-09 12:36:30 -0700

    g933-utils: adhere to strictDeps

--- home-manager/packages/g933-utils/default.nix
+++ home-manager/packages/g933-utils/default.nix
@@ -8,6 +8,7 @@ rustPlatform.buildRustPackage {
     rev = "b80cfd59fc41ae5d577c147311376dd7f7882493";
     sha256 = "06napzpk3nayzixb4l4fzdiwpgmcrsbc5j9m4qip1yn6dfkin3p0";
   };
-  buildInputs = [ pkgconfig libudev ];
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ libudev ];
   cargoSha256 = "091bdj90md7yhca8kcil970c1mgx1iap5lcmcqwg0miadbvwjzqy";
 }
