01339dc2df794feaabbfd2fa3dfc52e2782498e7
ash lea <example@thisismyactual.email>
2020-04-24 16:34:34 -0700

    boson: add mounts for zfs datasets these were previously mounted via zfs mountpoint option

--- devices/boson/hardware-configuration.nix
+++ devices/boson/hardware-configuration.nix
@@ -11,6 +11,13 @@
 
   fileSystems = {
     "/" = { device = "tank/root"; fsType = "zfs"; };
+    "/home" = { device = "tank/home"; fsType = "zfs"; };
+    "/home/ash" = { device = "tank/home/ash"; fsType = "zfs"; };
+    "/home/ash/tmp" = { device = "tank/home/ash/tmp"; fsType = "zfs"; };
+    "/home/ash/Projects" = { device = "tank/home/ash/projects"; fsType = "zfs"; };
+    "/home/ash/nextcloud" = { device = "tank/home/ash/nextcloud"; fsType = "zfs"; };
+    "/home/ash/.local/share/Steam" = { device = "tank/home/ash/steam"; fsType = "zfs"; };
+
     "/boot" = { device = "/dev/disk/by-uuid/FAC7-B487"; fsType = "vfat"; };
   };
 
