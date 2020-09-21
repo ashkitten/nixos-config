5be31dd5f4cdcffcb954488b34d67fa985abd3de
ash lea <example@thisismyactual.email>
2020-09-20 20:03:58 -0700

    boson: new fs configuration (with nvme!)

--- devices/boson/hardware-configuration.nix
+++ devices/boson/hardware-configuration.nix
@@ -4,33 +4,28 @@
   imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
 
   boot = {
-    initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" ];
+    initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
+    initrd.supportedFilesystems = [ ];
     kernelModules = [ "kvm-amd" "zfs" ];
     extraModulePackages = [ ];
   };
 
   fileSystems = {
-    "/" = { device = "tank/root"; fsType = "zfs"; };
+    "/" = { device = "/dev/disk/by-uuid/12ce6f08-182b-47de-95df-8bf73565d1c6"; fsType = "f2fs"; options = [ "compress_algorithm=zstd" ]; };
+    "/boot" = { device = "/dev/disk/by-uuid/D097-E4BD"; fsType = "vfat"; };
 
-    "/var/lib/stuff" = { device = "tank/stuff"; fsType = "zfs"; };
-
-    "/home" = { device = "tank/home"; fsType = "zfs"; };
+    "/home/ash/.local/share/Steam" = { device = "/bind/users/ash/steam"; options = [ "bind" ]; };
+    "/home/ash/games" = { device = "/bind/users/ash/games"; options = [ "bind" ]; };
+    "/home/ash/tmp" = { device = "/bind/users/ash/tmp"; options = [ "bind" ]; };
+    "/home/ash/.cache" = { device = "/bind/users/ash/cache"; options = [ "bind" ]; };
 
     "/home/ash" = { device = "tank/home/ash"; fsType = "zfs"; };
-    "/home/ash/.local/share/Steam" = { device = "tank/home/ash/steam"; fsType = "zfs"; };
     "/home/ash/Projects" = { device = "tank/home/ash/projects"; fsType = "zfs"; };
-    "/home/ash/games" = { device = "tank/home/ash/games"; fsType = "zfs"; };
     "/home/ash/nextcloud" = { device = "tank/home/ash/nextcloud"; fsType = "zfs"; };
-    "/home/ash/tmp" = { device = "tank/home/ash/tmp"; fsType = "zfs"; };
-
-    "/home/guest" = { device = "tank/home/guest"; fsType = "zfs"; };
-    "/home/guest/.local/share/Steam" = { device = "tank/home/guest/steam"; fsType = "zfs"; };
-
-    "/boot" = { device = "/dev/disk/by-uuid/FAC7-B487"; fsType = "vfat"; };
   };
 
   swapDevices = [
-    { device = "/dev/disk/by-uuid/585d6c99-7fb9-418f-b5e9-9c4c3151b904"; }
+    { device = "/swapfile"; }
   ];
 
   nix.maxJobs = 12;
