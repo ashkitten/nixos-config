1668ceb31292e5626ea8e99a4fa1b06bea490aca
ash lea <example@thisismyactual.email>
2021-01-20 14:17:36 -0800

    boson: add nofail to noncritical mounts

--- devices/boson/hardware-configuration.nix
+++ devices/boson/hardware-configuration.nix
@@ -14,14 +14,14 @@
     "/" = { device = "/dev/disk/by-uuid/b68651fb-9d82-43fc-b6e9-020ff5d4981d"; fsType = "xfs"; };
     "/boot" = { device = "/dev/disk/by-uuid/D097-E4BD"; fsType = "vfat"; };
 
-    "/home/ash/.local/share/Steam" = { device = "/bind/users/ash/steam"; options = [ "bind" ]; };
-    "/home/ash/games" = { device = "/bind/users/ash/games"; options = [ "bind" ]; };
-    "/home/ash/tmp" = { device = "/bind/users/ash/tmp"; options = [ "bind" ]; };
-    "/home/ash/.cache" = { device = "/bind/users/ash/cache"; options = [ "bind" ]; };
+    "/home/ash/.local/share/Steam" = { device = "/bind/users/ash/steam"; options = [ "bind" "nofail" ]; };
+    "/home/ash/games" = { device = "/bind/users/ash/games"; options = [ "bind" "nofail" ]; };
+    "/home/ash/tmp" = { device = "/bind/users/ash/tmp"; options = [ "bind" "nofail" ]; };
+    "/home/ash/.cache" = { device = "/bind/users/ash/cache"; options = [ "bind" "nofail" ]; };
 
-    "/home/ash" = { device = "tank/home/ash"; fsType = "zfs"; };
-    "/home/ash/Projects" = { device = "tank/home/ash/projects"; fsType = "zfs"; };
-    "/home/ash/nextcloud" = { device = "tank/home/ash/nextcloud"; fsType = "zfs"; };
+    "/home/ash" = { device = "tank/home/ash"; fsType = "zfs"; options = [ "nofail" ]; };
+    "/home/ash/Projects" = { device = "tank/home/ash/projects"; fsType = "zfs"; options = [ "nofail" ]; };
+    "/home/ash/nextcloud" = { device = "tank/home/ash/nextcloud"; fsType = "zfs"; options = [ "nofail" ]; };
   };
 
   swapDevices = [
